59ff90532647eb2df65737f598a8f48e32ea8b0c
Anton L <anton.loiko.engineer@gmail.com>
2026-05-29 22:21:20 -0500

    upd: main screen, add tabs. Import icon pack

---
+++ src/ui/main_menu/PanelManager.gd
@@ -0,0 +1,37 @@
+extends MarginContainer
+
+@onready var shop_button: Button = %ShopButton
+@onready var skills_button: Button = %SkillsButton
+@onready var home_button: Button = %HomeButton
+@onready var gear_button: Button = %GearButton
+@onready var base_button: Button = %BaseButton
+
+@onready var shop_panel: PanelContainer = %ShopPanel
+@onready var skills_panel: PanelContainer = %SkillsPanel
+@onready var home_panel: PanelContainer = %HomePanel
+@onready var gear_panel: PanelContainer = %GearPanel
+@onready var base_panel: PanelContainer = %BasePanel
+
+
+func _ready() -> void:
+	show_panel(home_panel)
+	home_button.grab_focus()
+	home_button.button_pressed = true
+
+	shop_button.pressed.connect(show_panel.bind(shop_panel))
+	skills_button.pressed.connect(show_panel.bind(skills_panel))
+	home_button.pressed.connect(show_panel.bind(home_panel))
+	gear_button.pressed.connect(show_panel.bind(gear_panel))
+	base_button.pressed.connect(show_panel.bind(base_panel))
+	
+
+func show_panel(panel_to_show: PanelContainer) -> void:
+	_hide_all_panels()
+	panel_to_show.show()
+
+func _hide_all_panels()->void:
+	shop_panel.hide()
+	skills_panel.hide()
+	home_panel.hide()
+	gear_panel.hide()
+	base_panel.hide()
