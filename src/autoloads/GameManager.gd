ac80711c64578c9355580eb75b1bf2cf96f96424
Anton L <anton.loiko.engineer@gmail.com>
2026-06-11 00:20:59 -0500

    upd: skills card/container/style

--- src/autoloads/GameManager.gd
+++ src/autoloads/GameManager.gd
@@ -10,6 +10,7 @@ const BASE_SPARK_CHANCE: float = 0.3
 const BASE_ENEMY_CHANCE: float = 0.1
 
 # --- Настройки навыков ---
+const SKILL_CHOICE_TRIGGERED_TRASHHOLD  = 15
 const SKILL_MAGNET_RADIUS: float = 120.0
 const SKILL_MAGNET_SPEED: float = 200.0
 const SKILL_SHADOW_BURN_MULT: float = 2.5
@@ -32,21 +33,21 @@ func get_spark_spawn_chance() -> float:
 # ------------------------------
 
 # --- Roguelike База Навыков ---
-const SKILLS_DB: Dictionary = {
+var SKILLS_DB: Dictionary = {
 	"magnet": {
-		"title": "МАГНИТ ИСКР",
+		"title": "+"+ str(SKILL_MAGNET_RADIUS) + " MAGNET RADIUS",
 		"icon": "res://src/assets/images/skills/skill_magnet.png", 
-		"color": Color(0.2, 0.5, 0.9) # Синий (Утилиты)
+		"bg_color": Color(0.281, 0.353, 0.676, 0.75) # Синий (Утилиты)
 	},
 	"light_shield": {
-		"title": "ЩИТ СВЕТА",
-		"icon": "res://src/assets/images/skillsskill_shield.png",
-		"color": Color(0.9, 0.7, 0.1) # Желтый (Свет/Защита)
+		"title": "+"+str(SKILL_SHIELD_DAMAGE_REDUCTION)+" REDUCTION",
+		"icon": "res://src/assets/images/skills/skill_shield.png",
+		"bg_color": Color(0.957, 0.773, 0.031, 0.749) # Желтый (Свет/Защита)
 	},
 	"shadow_burn": {
-		"title": "ОГНЕННАЯ АУРА",
-		"icon": "res://src/assets/images/skillsskill_fire.png",
-		"color": Color(0.8, 0.2, 0.2) # Красный (Атака)
+		"title": "+"+ str(SKILL_SHADOW_BURN_MULT)+" BURN",
+		"icon": "res://src/assets/images/skills/skill_fire.png",
+		"bg_color": Color(0.605, 0.049, 0.057, 0.75) # Красный (Атака)
 	}
 }
 
@@ -170,8 +171,8 @@ func add_sparks(amount: int) -> void:
 	sparks_changed.emit(sparks)
 	
 	run_sparks_counter += amount
-	if run_sparks_counter >= 50:
-		run_sparks_counter -= 50
+	if run_sparks_counter >= SKILL_CHOICE_TRIGGERED_TRASHHOLD:
+		run_sparks_counter -= SKILL_CHOICE_TRIGGERED_TRASHHOLD
 		skill_choice_triggered.emit()
 		
 	save_game()
--- src/core_loop/HUD/skill_choice/SkillCard.gd
+++ src/core_loop/HUD/skill_choice/SkillCard.gd
@@ -5,14 +5,14 @@ signal card_selected(id: String)
 
 var skill_id: String = ""
 
-@onready var bg_color: ColorRect = %BgColor
+@onready var bg_color: Panel = %BgColor
 @onready var icon_rect: TextureRect = %IconRect
 @onready var title_label: Label = %TitleLabel
-@onready var highlight: ReferenceRect = %Highlight
+@onready var highlight: Panel = %Highlight
 
 func _ready() -> void:
 	pressed.connect(_on_pressed)
-	highlight.hide()
+	#highlight.hide()
 	
 	# Устанавливаем точку трансформации в центр для красивого масштабирования
 	pivot_offset = size / 2.0
@@ -20,7 +20,13 @@ func _ready() -> void:
 func setup(id: String, data: Dictionary) -> void:
 	skill_id = id
 	title_label.text = data["title"]
-	bg_color.color = data["color"]
+	
+	# Дублируем StyleBoxFlat, чтобы изменение цвета одной карточки не ломало остальные
+	var base_style = highlight.get_theme_stylebox("panel")
+	if base_style:
+		var new_style = base_style.duplicate() as StyleBoxFlat
+		new_style.bg_color = data["bg_color"]
+		highlight.add_theme_stylebox_override("panel", new_style)
 	
 	if ResourceLoader.exists(data["icon"]):
 		icon_rect.texture = load(data["icon"])
@@ -28,17 +34,17 @@ func setup(id: String, data: Dictionary) -> void:
 func _on_pressed() -> void:
 	disabled = true 
 	
-	# Визуальная отдача (вдавливание + белый контур)
-	highlight.show()
-	var tween = create_tween().set_trans(Tween.TRANS_QUAD)
+	# Визуальная отдача (вдавливание + неоновый контур)
+	#highlight.show()
+	var tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
 	
-	# Уменьшаем карточку
-	tween.tween_property(self, "scale", Vector2(0.9, 0.9), 0.1)
+	# Уменьшаем карточку (эффект нажатия)
+	tween.tween_property(self, "scale", Vector2(0.9, 0.9), 0.2)
 	# Возвращаем размер
 	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
 	
-	# Даем небольшую задержку (0.3с), чтобы игрок успел насладиться эффектом нажатия
-	tween.tween_interval(0.3)
+	# Даем небольшую задержку (0.3с) для завершения анимации перед отправкой сигнала
+	tween.tween_interval(0.5)
 	tween.tween_callback(func():
 		card_selected.emit(skill_id)
 	)
